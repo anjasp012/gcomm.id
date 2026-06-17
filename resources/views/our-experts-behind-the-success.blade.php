6e8e584073375483ebc89934f98b5cdffbe2d724
unknown <anjasputra012@gmail.com>
2026-06-17 08:25:02 +0700

    first commit

--- resources/views/our-experts-behind-the-success.blade.php
+++ resources/views/our-experts-behind-the-success.blade.php
@@ -47,7 +47,7 @@
 
         <!-- Expert 3 -->
         <div class="group relative flex w-[280px] h-[400px] bg-black rounded-[20px] overflow-hidden items-end justify-center transition-all duration-300 ease-out shadow-lg hover:shadow-2xl">
-            <div class="absolute -inset-5 w-[320px] h-[440px] bg-cover bg-center bg-no-repeat transition-all duration-500 ease-out group-hover:scale-105" style="background-image: url('{{ asset('images/teams/nurlaila.JPG') }}')"></div>
+            <div class="absolute -inset-10 w-[320px] h-[440px] bg-cover bg-center bg-no-repeat transition-all duration-500 ease-out group-hover:scale-105" style="background-image: url('{{ asset('images/teams/nurlaila.JPG') }}')"></div>
             <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-86 group-hover:opacity-35 transition-all duration-300 ease-out"></div>
             <div class="p-4 w-full text-center relative z-10 translate-y-[-15px] group-hover:translate-y-0 transition-transform duration-500 ease-out">
                 <h3 class="text-white text-[18px] font-medium mt-2 text-center font-display">Nurlaila</h3>
