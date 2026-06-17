0b0af6d64e0a258040be734da8a94b4f277d17e2
unknown <anjasputra012@gmail.com>
2026-06-17 13:10:33 +0700

    'oke'

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,13 +9,22 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Izinkan semua user yang terdaftar untuk mengakses panel admin
+        // Secara default Filament memblokir akses di server production (VPS) jika method ini tidak ada
+        return true; 
+    }
 
     /**
      * Get the attributes that should be cast.
