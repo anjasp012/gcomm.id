ccda61598e0f9c0dd40adaf291c92421c94cb881
unknown <anjasputra012@gmail.com>
2026-06-17 13:13:28 +0700

    'oke'

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'clients' => \App\Models\Client::all()
+    ]);
 });
 
 Route::get('/about-gcomm', function () {
